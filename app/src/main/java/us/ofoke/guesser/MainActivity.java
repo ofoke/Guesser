e4cf684a873df58ec74ab15cc60ad426f2960ce8
Art <imartholland@gmail.com>
2016-06-30 18:04:28 -0700

    Initial commit

---
+++ app/src/main/java/us/ofoke/guesser/MainActivity.java
@@ -0,0 +1,15 @@
+package us.ofoke.guesser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    public static final String TAG = "guesser";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
